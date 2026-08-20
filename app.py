ee13ec01f6b28437626558be1f67ed28749c5bd9
sanchitasoni00 <sonisanchita00@gmail.com>
2026-08-21 05:22:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -442,11 +442,9 @@ def home_page():
 # =========================================================
 
 def User_dashboard():
-       st.title("USER DASHBOARD TEST")
-       st.write("If you can see this, the dashboard is loading correctly.")
+    st.title("USER DASHBOARD TEST")
+    st.write("If you can see this, the dashboard is loading correctly.")
     
-    
-
     st.title("👨‍🎓 User Dashboard")
 
 
