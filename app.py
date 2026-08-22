5b07948b67faadb64ae5eb1eea8e6fc0cf992991
sanchitasoni00 <sonisanchita00@gmail.com>
2026-08-22 07:58:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -783,11 +783,8 @@ if not my_meals:
         )
 
     else:
-
         for meal in my_meals:
-
             with st.container(border=True):
-
                 col1, col2, col3 = st.columns(
                     [3, 1, 1]
                 )
