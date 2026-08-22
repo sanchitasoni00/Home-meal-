5428d583558efb4568184f3219febf7583f1398b
sanchitasoni00 <sonisanchita00@gmail.com>
2026-08-22 08:21:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ if not os.path.exists(DATABASE_FILE):
         with open(DATABASE_FILE, "w", encoding="utf-8") as file:
             json.dump(default_database, file, indent=4)
 
-        return default_database
+    return default_database
 try:
     with open(DATABASE_FILE, "r", encoding="utf-8") as file:
         database = json.load(file)
